d3623959c4c5bb1f3a57d3fa8ff41f1ee3d5fd6a
enieves <enieves@106-02.info2>
2023-06-05 10:17:54 +0200

    hecho?

--- TEMA8 RECUPERACION/src/ej3/Main.java
+++ TEMA8 RECUPERACION/src/ej3/Main.java
@@ -1,8 +1,139 @@
 package ej3;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		int opcion=1;
+		Scanner sc=new Scanner(System.in);
+		//coleccion de producto
+		TreeSet<Producto> productos = new TreeSet<Producto>();
+		String nombre = "";
+		double precio = 0;
+		int caducar=0;
+		String tipo="";
+		Producto producto=null;
+		
+		String tipoProd="";
+		
+		while(opcion!=0) {
+			
+
+			System.out.println("introduce la opcion: ");
+			
+			opcion=sc.nextInt();
+			
+			switch (opcion) {
+			//
+			case 1:{
+				System.out.println("introduce el nombre: ");
+				nombre=sc.next();
+				sc.nextLine();
+				
+				System.out.println("introduce el precio: ");
+				precio=sc.nextDouble();
+				sc.nextLine();
+				
+				System.out.println("introduce el tipo: ");
+				tipoProd=sc.nextLine();
+				//
+				
+				//si el producto es perecedero
+				if(tipoProd.equals("Perecedero")) {
+
+					System.out.println("introduce dias caducar: ");
+					//como es perecedero y tiene el elemento caducar de más
+					caducar=sc.nextInt();
+					sc.nextLine();
+					//
+					producto=new Perecedero(nombre,precio,caducar);
+					
+				}
+	
+				//si el producto no es perecedero
+				else if(tipoProd.equals("No perecedero")) {
+
+					System.out.println("introduce tipo: ");
+					tipo=sc.next();
+					sc.nextLine();
+					
+					//
+					producto=new NoPerecedero(nombre,precio,tipo);
+					
+					
+				}
+				else {
+					System.out.println("Tipo no valido");
+					break;
+				}
+				
+				
+				
+				
+				
+				//añade el producto
+				productos.add(producto);
+				
+				break;
+			}//c1
+			
+			case 2:{
+				System.out.println(productos);
+				
+				break;
+			}//c2
+			
+			case 3:{
+				System.out.println("introduce el nombre: ");
+				System.out.println("introduce el precio: ");
+				nombre=sc.next();
+				sc.nextLine();
+				precio=sc.nextDouble();
+				sc.nextLine();
+				
+				System.out.println("introduce el tipo: ");
+				tipoProd=sc.nextLine();
+				
+				//si el producto es perecedero
+				if(tipoProd.equals("Perecedero")) {
+					//
+					caducar=sc.nextInt();
+					//
+					producto=new Perecedero(nombre,precio,caducar);
+					
+				}
+	
+				//si el producto no es perecedero
+				else if(tipoProd.equals("No perecedero")) {
+	
+					//
+					producto=new NoPerecedero(nombre,precio,tipo);
+					
+					
+				}
+				else {
+					System.out.println("Tipo no valido");
+				}
+				
+				productos.remove(tipoProd);
+				
+				break;
+			}//c3
+	
+			//salir
+			case 0:{
+				
+				break;
+			}//c0
+			
+				
+			}//swotch
+			
+		}
+		
+		
 		
 		
 	}//main method
